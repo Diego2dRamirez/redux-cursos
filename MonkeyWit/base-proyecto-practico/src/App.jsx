1507952a647b4eb602ee66fe1e2e7c33f160dcdd
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-12-17 21:23:52 -0600

    primeros-pasos-Monkey: Instalación de React, Redux-toolkit, Ant Design & Axios

---
+++ MonkeyWit/base-proyecto-practico/src/App.jsx
@@ -0,0 +1,14 @@
+import { Alert } from "antd";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React JS + Redux Toolkit</h1>
+      <Alert title="Ant Design install" type="success" showIcon />
+    </>
+  )
+};
+
+
+export default App;
+
